8695c3f70ed247cc14da63e95f713d849bae9af2
Melina Acuña <melinaacuna88@gmail.com>
2023-06-26 10:57:06 -0700

    PreferredColorScheme to light

--- mazeApp/ContentView.swift
+++ mazeApp/ContentView.swift
@@ -21,5 +21,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
     }
 }
+
